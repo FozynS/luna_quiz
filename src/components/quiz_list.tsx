078c4a5c2b5426c0e2082840bc0a327e56c5a46f
Danylo Shvydkyi <wolf44488@gmail.com>
2024-05-23 19:42:00 +0200

    Add function for edit and take quiz

--- src/components/quiz_list.tsx
+++ src/components/quiz_list.tsx
@@ -31,6 +31,10 @@ const QuizList: React.FC = () => {
   const [quizes, setQuizes] = useState<any[]>(testQuiz);
   const [takingQuiz, setTakingQuiz] = useState<any | null>(null);
 
+  const handleEditQuiz = (quiz: any) => {
+    console.log(quiz);
+  }
+
   const handleDeleteQuiz = (quizIdToDelete: number) => {
     setQuizes((prevState) => prevState.filter((quiz) => quiz.id !== quizIdToDelete));
   }
@@ -47,13 +51,14 @@ const QuizList: React.FC = () => {
       </nav>
 
       <div>
+
         <ul className="divide-y divide-gray-100 px-4">
           {quizes.map((quiz) => (
             <li key={quiz.id} className="flex justify-between gap-x-6 py-5">
               <p className="text-xl	font-semibold text-gray-900 text-center">{quiz.title}</p>
               <div className="flex min-w-0 gap-x-4">
-                <button>Edit</button>
-                <button onClick={() => {handleDeleteQuiz(quiz.id)}}>Delete</button>
+                <button onClick={() => handleEditQuiz(quiz)}>Edit</button>
+                <button onClick={() => handleDeleteQuiz(quiz.id)}>Delete</button>
                 <button onClick={() => handleTakeQuiz(quiz)}>Take Quiz</button>
               </div>
             </li>
